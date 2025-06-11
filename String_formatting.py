5a7fd80fdb83a779f16b1e850a626aa6fb929942
Kostyantyn <shtoykokostya8@gmail.com>
2025-06-11 23:37:41 +0200

    String_formatting

--- String_formatting.py
+++ String_formatting.py
@@ -3,11 +3,39 @@
 # age = 19
 # print('Kostya is ' + str(age) + 'years old')
 # print('Kostya is ' + str(23) + 'years old')
-name = 'Kostya'
-age = 23
-name_and_age = 'My name is {0} I\'m {1} years old'.format(name,age)
-print(name_and_age)
-name_and_age = 'My name is {0} I\'m {1} years old'.format('Kostya','20')
-print(name_and_age)
-name_and_age = 'My name is {} I\'m {} years old'.format('Kostya','20')
-print(name_and_age)
+
+# name = 'Kostya'
+# age = 23
+# name_and_age = 'My name is {0} I\'m {1} years old'.format(name,age)
+# print(name_and_age)
+# name_and_age = 'My name is {0} I\'m {1} years old'.format('Kostya','20')
+# print(name_and_age)
+# name_and_age = 'My name is {} I\'m {} years old'.format('Kostya','20')
+# print(name_and_age)
+
+# float_result = 1000/7
+# print(float_result)
+# print('The result of division is {0:1.4f}'.format(float_result))
+# print('The result of division is {0:10.4f}'.format(float_result))
+#
+# print('''
+# {0} {1} {2}
+# {3} {4} {5}
+# {6} {7} {8}
+# '''.format(1.432, 345.12413, 4677.43333321, 7.434332,
+#            32.12413, 77.4233321, 9.432, 345.12413, 4677,))
+#
+# print('''
+# {0:10.2f} {1:10.2f} {2:10.2f}
+# {3:10.2f} {4:10.2f} {5:10.2f}
+# {6:10.2f} {7:10.2f} {8:10.2f}
+# '''.format(1.432, 345.12413, 4677.43333321, 7.434332,
+#            32.12413, 77.4233321, 9.432, 345.12413, 4677,))
+
+# Homework
+print('''
+{0:15.4f}{1:15.4f}{2:15.4f}{3:15.4f}
+{4:15.4f}{5:15.4f}{6:15.4f}{7:15.4f}
+'''.format(12.42, 15.1213, 467.43333321, 7.43432,
+           1992.12413, 17.4233321, 4.432, 345.123413))
+
