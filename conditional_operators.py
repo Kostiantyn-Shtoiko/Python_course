bbb8bc0bdcbe79de05567b3f78477a8f6d042c38
Kostyantyn <shtoykokostya8@gmail.com>
2026-05-14 18:54:59 +0200

    conditional_operators

---
+++ conditional_operators.py
@@ -0,0 +1,52 @@
+# x = 3
+#
+# if x > 3:
+#     print('x > 3')
+# elif x < 3:
+#     print('x < 3')
+# else:
+#     print('x == 3')
+
+# day_time = 'afternoon'
+#
+# if day_time == 'morning':
+#     print('Monster wakes up')
+# elif day_time == 'afternoon':
+#     print('Monster is walking')
+# elif day_time == 'evening':
+#     print('Monster is eating')
+# elif day_time == 'night':
+#     print('Monster is sleeping')
+# else:
+#     print('Monster is doing something')
+
+# x = 32
+# if x % 2 == 0:
+#     print('x is even')
+# else:
+#     print('x is odd')
+# print('Some text')
+
+# user_input = input('Input something ')
+# if user_input == 'Hello':
+#     print('Hello! Nice to meet you')
+
+# lucky_number = input('Enter a number')
+# if lucky_number:
+#     print(lucky_number + ' Is your lucky number! ')
+# else:
+#     print('You have to enter some number, please try again ')
+
+# Homework
+
+# numbers = int(input(' Enter any number '))
+# if numbers == 7:
+#     print('7 is a lucky number! Today is your lucky day!')
+# else:
+#     print('Thank you! Have a nice day! ')
+
+numbers = int(input('Enter an integer number '))
+if numbers % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
