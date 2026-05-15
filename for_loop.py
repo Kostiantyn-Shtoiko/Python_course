1280356f0db78a6cc0564229af9c281123609452
Kostyantyn <shtoykokostya8@gmail.com>
2026-05-15 19:30:12 +0200

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,45 @@
+number_list = [1,2,3,4,5,6,7,8,9,10]
+# for number in number_list:
+#     print(number)
+#     print('Hi!')
+
+# for number in number_list:
+#     if number % 2 == 0:
+#         print(number)
+#     else:
+#         print('Hey!')
+# list_number_sum = 0
+# for number in number_list:
+#     list_number_sum = list_number_sum + number
+# print(list_number_sum)
+
+# greeting = 'Hello Python!'
+#
+# for letter in greeting:
+#     if letter != 'o':
+#         print(letter)
+
+# dictionary = {'key1': 'value1','key2': 'value2','key3': 'value3',}
+# for item in dictionary:
+#     print(item)
+# for item in dictionary.items():
+#     print(item)
+# for item in dictionary.keys():
+#     print(item)
+# for item in dictionary.values():
+#     print(item)
+
+# for _ in range(5):
+#     print('Hello!')
+
+# Homework
+# number_sum = 0
+# for x in range(10,30):
+#     if x % 2 ==0:
+#         number_sum = number_sum + x
+# print(number_sum)
+
+user_input = int(input('Input number '))
+for x in range(user_input):
+    print('Hello!')
+
