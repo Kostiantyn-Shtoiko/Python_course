b24504d908e8397c319a3d1425fdbc7f6ba453fd
Kostyantyn <shtoykokostya8@gmail.com>
2026-05-18 23:26:03 +0200

    nested_loops

---
+++ nested_loops.py
@@ -0,0 +1,21 @@
+from itertools import count
+
+# smile = '\U0001F600'
+# for number in range(10):
+#     count = 0
+#     emoticons = ''
+#     while count <= number:
+#         emoticons += '\U0001F600'
+#         count += 1
+#     print(emoticons)
+
+# smile = '\U0001F600'
+# for number in range(10):
+#     count = 0
+#     emoticons = ''
+#     for count in range(number + 1):
+#         emoticons += '\U0001F600'
+#     print(emoticons)
+
+for number in range(1,11):
+    print('\U0001F600' * number)
