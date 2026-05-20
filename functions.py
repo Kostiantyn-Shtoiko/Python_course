43e99cef33d4ff2fd83d970548e57d95e45a5ba2
Kostyantyn <shtoykokostya8@gmail.com>
2026-05-20 23:09:42 +0200

    functions_1

---
+++ functions.py
@@ -0,0 +1,15 @@
+# x = print('Hello!')
+# y = set()
+#
+# print(type(x))
+# print(type(y))
+#
+# print(x)
+# print(y)
+
+
+my_list = [1,2,3]
+my_list.append(4)
+print(my_list)
+my_list.clear()
+print(my_list)
