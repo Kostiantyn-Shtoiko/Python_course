2590bece87cdfe8f468615bbfa4616c977aa8678
Kostya <@>
2026-05-23 23:49:01 +0200

    functions 0.1

--- functions.py
+++ functions.py
@@ -8,8 +8,26 @@
 # print(y)
 
 
-my_list = [1,2,3]
-my_list.append(4)
-print(my_list)
-my_list.clear()
-print(my_list)
+# my_list = [1,2,3]
+# my_list.append(4)
+# print(my_list)
+# my_list.clear()
+# print(my_list)
+
+# def print_greeting():
+#     '''
+#     Print a greeting
+#     :return: None
+#     '''
+#     print("Hello World")
+# print_greeting()
+# help(print_greeting)
+
+def print_greeting_with_name(name):
+    '''
+    :param name
+    :return: None
+    '''
+    print("Hello " + name + '!')
+print_greeting_with_name('John')
+help(print_greeting_with_name)
