179dd917e1dba4de46ad0b78c12bf3688b5cb95c
Kostya <shtoykokostya8@gmail.com>
2026-05-25 19:12:22 +0200

    oop_start

---
+++ oop_intro.py
@@ -0,0 +1,10 @@
+# help(list)
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# my_list.append(5)
+# print(my_list)
+
+class MyFirstClass:
+    pass
+
+object_of_my_class = MyFirstClass()
+print(type(object_of_my_class))
