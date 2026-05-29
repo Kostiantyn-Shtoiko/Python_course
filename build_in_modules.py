1f28c4205cf03debd7c79c99129258d3c670354a
Kostya <shtoykokostya8@gmail.com>
2026-05-29 14:29:57 +0200

    build_in_modules

---
+++ build_in_modules.py
@@ -0,0 +1,27 @@
+# import random
+# x = random.randint(1,10)
+# print(x)
+#
+# from random import randint
+# x = randint(1,10)
+# print(x)
+
+from random import shuffle as shuffle_my_list
+my_list = [1,2,3,4,5,6]
+shuffle_my_list(my_list)
+print(my_list)
+
+# Homework
+import math
+
+x = 123456789
+result = math.sqrt(x)
+print(result)
+
+n =  987
+result_1 = math.factorial(n)
+print(result_1)
+
+
+result_2 = math.pow(876, 54)
+print(result_2)
