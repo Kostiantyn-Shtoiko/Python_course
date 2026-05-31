65fc9fa1f40c3831cf5d8b0b79ac9998dc9a01a9
Kostya <shtoykokostya8@gmail.com>
2026-05-31 20:54:03 +0200

    binary_intro

---
+++ Text/binary_intro.py
@@ -0,0 +1,22 @@
+x = 127
+print(1 * pow(10,2) + 2 * pow(10,1) + 7 * pow(10,0))
+
+y = 1035
+print(1 * pow(10,3) + 0 * pow(10,2) + 3 * pow(10,1) + 5 * pow(10,0))
+
+x = 0b101
+print(x)
+print(1 * pow(2,2) + 0 * pow(2,1) + 1 * pow(2,0))
+
+y = 0b0110
+print(y)
+print(0 * pow(2,3) + 1 * pow(2,2) + 1 * pow(2,1) + 0 * pow(2,0))
+
+z = 0x11
+print(z)
+print(1 * pow(16,1) + 1 * pow(16,0))
+
+z = 0xffff
+print(z)
+print(format(z, '0>42b'))
+
