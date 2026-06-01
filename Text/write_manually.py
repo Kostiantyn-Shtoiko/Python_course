5c78c24b6f76c1f3d7504a4b9ed140998bfb0c71
Kostya <shtoykokostya8@gmail.com>
2026-06-01 19:09:20 +0200

    write_manually

---
+++ Text/write_manually.py
@@ -0,0 +1,7 @@
+with open('test_binary', 'bw') as test_file:
+    for number in range(21):
+        test_file.write(bytes([number]))
+
+with open('test_binary', 'br') as test_file:
+    for number in test_file:
+        print(number)
