07228d967d8e042c9c85c3f7161216a10eb1d473
Kostya <shtoykokostya8@gmail.com>
2026-06-04 20:28:04 +0200

    Error

---
+++ error/error_handling.py
@@ -0,0 +1,27 @@
+# SyntaxError
+# des function_1():
+#     x = 1+1
+#     print(x)
+
+# NameErrors
+# print(x)
+
+# TypeError
+# len(1)
+# my_list = [1,2,3]
+# print(my_list+ 'hello')
+
+# IndexError
+# my_list = [1,2,3]
+# print(my_list[4])
+
+# ValueErrors
+# print(int('43'))
+# print(int('Hello'))
+
+# KeyError
+# my_dict = {'first': 'apple', 'second': 'banana'}
+# my_dict['third']
+
+# AttributeError
+'red'.attr()
